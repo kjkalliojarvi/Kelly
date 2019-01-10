24d61c39fe1c5f4312c56366fe566d503c22887c
Kari Kalliojarvi <kari.kalliojarvi@gmail.com>
2019-01-10 16:11:13 +0200

    Update

--- Kelly/Tanaan.py
+++
@@ -1,9 +0,0 @@
-import data
-import datetime
-
-kaikki_ravit = data.listat()
-pvm = datetime.datetime.now().strftime('%d.%m.%Y')
-for ravit in kaikki_ravit:
-    if ravit['date'] == pvm:
-        a = ravit.find('pool')['file'].split('_')
-        print(ravit['name'], ravit['code'], ravit['track-code'], a[0])
